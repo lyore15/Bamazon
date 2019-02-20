e23dea4b98d7e8e6fb7e81575a0d29b712dcd552
lyore15 <regjlyons@gmail.com>
2019-02-20 08:10:41 -0500

    lots of changes

--- customer.js
+++ customer.js
@@ -18,7 +18,6 @@ function start() {
         if (err) throw err;
         console.log("Welcome to Bamazon!");
 
-
         var table = new Table({
             head: ["Item ID", "Product Name", "Department", "Price", "Stock Quantity"],
             style: {
@@ -32,70 +31,71 @@ function start() {
                 [res[i].ItemID, res[i].ProductName, res[i].DepartmentName, res[i].Price, res[i].StockQuantity]
             );
         }
-        start();
-    });
-
-
-    // Inquirer 
-}function startPrompt() {
-
-    inquirer.prompt([{
-
-        type: "name",
-        name: "Bamazon",
-        message: "Welcome to Bamazon!"
-    }]);
-
-    connection.query('SELECT * FROM Products', function (err, res) {
-        if (err) {
-            console.log(err);
-        }
-
-        for (var i = 0; i < res.length; i++) {
-            console.log("\nID: " + res[i].ItemID + " | " + "Product: " + res[i].ProductName + " | " + "Department: " + res[i].DepartmentName + " | " + "Price: " + res[i].Price + " | " + "QTY: " + res[i].StockQuantity);
-            console.log("-------------------------------------------------------------------------")
-        }
-    })
     console.log(table.toString());
-
-    // Prompt user to select a product and enter desired quantity
-    function buyPrompt() {
-        inquirer.prompt([{
-            name: "itemId",
-            type: "input",
-            message: "Enter the id of the product you'd like to buy."
-        }, {
-            name: "quantity",
-            type: "input",
-            message: "How many would you like to purchase?"
-        }]).then(function (answer) {
-            for (var i = 0; i < res.length; i++) {
-                if (res[i].ItemId === parseInt(answer.ItemId)) {
-                    // If order quantity is too high, notify user of insufficient stock
-                    if (res[i].StockQuantity < parseInt(answer.Quantity)) {
-                        console.log("Insufficient stock!");
-                        buyPrompt();
-                    } else {
-                        // Calculate order total and remaining stock
-                        var total = parseFloat(answer.Quantity * res[i].Price).toFixed(2);
-                        var newStock = res[i].StockQuantity - answer.Quantity;
-
-                        // Construct query to update stock
-                        var updateStock = 'UPDATE `products` SET `stock_quantity` = ' + newStock + ' WHERE `item_id` = ' + answer.ItemId
-                        connection.query(updateStock, function (err, res) {
-                            if (err) {
-                                console.log(err);
-                            } else {
-                                console.log(res.affectedRows + " product updated");
-                            }
-                        });
-
-                        // Notify user of successful purchase
-                        console.log("You have purchased " + answer.Quantity + " " + res[i].ProductName);
-                        console.log("Your order total is " + total);
-                    }
-                }
-            }
-        });
-    }
+    });
 }
+
+start();
+    // Inquirer 
+// }function startPrompt() {
+
+//     inquirer.prompt([{
+
+//         type: "name",
+//         name: "Bamazon",
+//         message: "Welcome to Bamazon!"
+//     }]);
+
+//     connection.query('SELECT * FROM Products', function (err, res) {
+//         if (err) {
+//             console.log(err);
+//         }
+
+//         for (var i = 0; i < res.length; i++) {
+//             console.log("\nID: " + res[i].ItemID + " | " + "Product: " + res[i].ProductName + " | " + "Department: " + res[i].DepartmentName + " | " + "Price: " + res[i].Price + " | " + "QTY: " + res[i].StockQuantity);
+//             console.log("-------------------------------------------------------------------------")
+//         }
+//     })
+//     console.log(table.toString());
+
+//     // Prompt user to select a product and enter desired quantity
+//     function buyPrompt() {
+//         inquirer.prompt([{
+//             name: "itemId",
+//             type: "input",
+//             message: "Enter the id of the product you'd like to buy."
+//         }, {
+//             name: "quantity",
+//             type: "input",
+//             message: "How many would you like to purchase?"
+//         }]).then(function (answer) {
+//             for (var i = 0; i < res.length; i++) {
+//                 if (res[i].ItemId === parseInt(answer.ItemId)) {
+//                     // If order quantity is too high, notify user of insufficient stock
+//                     if (res[i].StockQuantity < parseInt(answer.Quantity)) {
+//                         console.log("Insufficient stock!");
+//                         buyPrompt();
+//                     } else {
+//                         // Calculate order total and remaining stock
+//                         var total = parseFloat(answer.Quantity * res[i].Price).toFixed(2);
+//                         var newStock = res[i].StockQuantity - answer.Quantity;
+
+//                         // Construct query to update stock
+//                         var updateStock = 'UPDATE `products` SET `stock_quantity` = ' + newStock + ' WHERE `item_id` = ' + answer.ItemId
+//                         connection.query(updateStock, function (err, res) {
+//                             if (err) {
+//                                 console.log(err);
+//                             } else {
+//                                 console.log(res.affectedRows + " product updated");
+//                             }
+//                         });
+
+//                         // Notify user of successful purchase
+//                         console.log("You have purchased " + answer.Quantity + " " + res[i].ProductName);
+//                         console.log("Your order total is " + total);
+//                     }
+//                 }
+//             }
+//         });
+//     }
+// }
